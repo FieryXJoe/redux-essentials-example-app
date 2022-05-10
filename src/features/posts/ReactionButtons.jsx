e4a7871a17cc29764f73afe5f99fc1eb51882bf1
FieryXJoe <josephmsherry@comcast.net>
2022-05-10 05:25:11 -0400

    format changes in bugfix attempt

--- src/features/posts/ReactionButtons.jsx
+++ src/features/posts/ReactionButtons.jsx
@@ -15,9 +15,12 @@ export const ReactionButtons = ({ post }) => {
     const dispatch = useDispatch();
   const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
     return (
-      <button key={name} type="button" className="muted-button reaction-button"
+      <button 
+      key={name}
+      type="button"
+      className="muted-button reaction-button"
       onClick ={() =>
-        dispatch(reactionAdded({ postId: post.id, reaaction: name}))
+        dispatch(reactionAdded({ postId: post.id, reaction: name}))
       }>
         {emoji} {post.reactions[name]}
       </button>
